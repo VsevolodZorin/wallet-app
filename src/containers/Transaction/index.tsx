fd5b232869dceb2e63890c5eca675b61ab3afb10
Vsevolod Zorin <vsevolod.dev@gmail.com>
2023-03-11 11:08:12 +0200

    add logic on Transaction page

--- src/containers/Transaction/index.tsx
+++ src/containers/Transaction/index.tsx
@@ -4,15 +4,16 @@ import { FC, useCallback } from 'react';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
 import { useAppSelector } from 'src/hooks/redux';
-import { selectTransactionById } from 'src/store/selectors';
+import { TransactionTypeEnum } from 'src/store/reducers/app.slice';
+import { selectCardBalance, selectTransactionById } from 'src/store/selectors';
 import { getDate } from 'src/utils/get-date';
 import { getTime } from 'src/utils/get-time';
 import './transaction.scss';
 
 const Transaction: FC = () => {
   const { transactionId } = useParams();
-
   const transaction = useAppSelector(state => selectTransactionById(state, +transactionId!));
+  const cardBalance = useAppSelector(selectCardBalance);
 
   const renderTransaction = useCallback(() => {
     if (transaction) {
@@ -24,21 +25,27 @@ const Transaction: FC = () => {
             </Link>
           </div>
           <div className="transaction__top-part">
-            <span className="transaction__top-part--sum">$7.50</span>
-            <span>Airalo</span>
+            <span className="transaction__top-part--sum">
+              {transaction.type === TransactionTypeEnum.PAYMENT && '+'}${transaction.sum}
+            </span>
+            <span>{transaction.name || transaction.authorizedUser}</span>
             <span>
               {getDate(new Date(transaction.date)) + ', ' + getTime(new Date(transaction.date))}
             </span>
           </div>
           <div className="transaction__bottom-part ui-block">
             <div className="transaction__bottom-part--status">
-              <span className="transaction__text--title">Status: Approved</span>
+              <span className="transaction__text--title">
+                Status: {transaction.pending ? 'Pending' : 'Approved'}
+              </span>
               <span>RBC Bank Debit Card</span>
             </div>
             <hr className="transaction__bottom-part--line" />
             <div className="transaction__bottom-part--total">
               <span className="transaction__text--title">Total</span>
-              <span className="transaction__text--title">$7.50</span>
+              <span className="transaction__text--title">
+                ${transaction.pending ? null : cardBalance}
+              </span>
             </div>
           </div>
         </>
